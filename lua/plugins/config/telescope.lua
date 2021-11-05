dc0f2f3ccfc93035578c1f1b32cfa22772411d34
Nil <nil@lin.am>
2021-11-05 17:20:08 +0800

    clear up

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -2,7 +2,6 @@ require('telescope').setup{
   defaults = {
     selection_caret = '  ',
     sorting_strategy = 'ascending',
-    -- prompt_prefix = '   ',
     prompt_prefix = ' 😎 ',
     layout_config = {
       mirror = false,
@@ -26,47 +25,6 @@ require('telescope').setup{
     },
   }, -- end of pickers
 
-  -- layout_config = {
-  --   vimgrep_arguments = {
-  --     'rg',
-  --     '--color=never',
-  --     '--no-heading',
-  --     '--with-filename',
-  --     '--line-number',
-  --     '--column',
-  --     '--smart-case'
-  --   },
-  --   prompt_position = "bottom",
-  --   prompt_prefix = "  ",
-  --   selection_caret = "  ",
-  --   initial_mode = "insert",
-  --   selection_strategy = "reset",
-  --   sorting_strategy = "descending",
-  --   layout_strategy = "horizontal",
-  --   layout_defaults = {
-  --     -- TODO add builtin options.
-  --   },
-  --   file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-  --   file_ignore_patterns = {".git/.*"},
-  --   generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-  --   shorten_path = true,
-  --   winblend = 0,
-  --   width = 0.75,
-  --   preview_cutoff = 120,
-  --   results_height = 1,
-  --   results_width = 0.8,
-  --   border = {},
-  --   borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
-  --   color_devicons = true,
-  --   use_less = true,
-  --   set_env = { ['COLORTERM'] = 'truecolor' },
-  --   file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-  --   grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-  --   qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
-  --   -- Developer configurations: Not meant for general override
-  --   buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  -- },
   extensions = {
     fzf = {
       fuzzy = true,                    -- false will only do exact matching
